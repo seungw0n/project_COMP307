4db9cb03e2c7a67c434191fc3a2c691dc9435870
SEUNG WON JOENG <stevenswjoeng@gmail.com>
2020-04-20 00:48:19 +0900

    Fix merged errors

--- project/settings.py
+++ project/settings.py
@@ -149,11 +149,7 @@ STATIC_ROOT = '/media/'
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = '/Users/victormassenet/OneDrive/Documents/McGill_S6/COMP307/project/project_COMP307'
-<<<<<<< HEAD
 # MEDIA_ROOT = '/Users/Greg/Desktop/McGill/U5/Winter 2020/COMP307/Project/new project repo/market/media'
 MEDIA_ROOT = '/Users/joeng-seungwon/github/project_COMP307/market/media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-=======
-MEDIA_ROOT = '/Users/Greg/Desktop/McGill/U5/Winter 2020/COMP307/Project/new project repo/market/media'
->>>>>>> f9db38b0da6d50f9fb77c152043aec6969cbcdda
