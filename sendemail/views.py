e77198b78d360e69fd02992bea757666d6895edb
SEUNG WON JOENG <stevenswjoeng@gmail.com>
2020-04-22 22:10:40 +0900

    Fix errors

--- sendemail/views.py
+++ sendemail/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import ContactForm
+from django.conf import settings
 
 # Create your views here.
 def contactView(request):
@@ -13,9 +14,10 @@ def contactView(request):
             subject = form.cleaned_data['subject']
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
+            to_email = [settings.EMAIL_HOST_USER,]
             try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
-                send_mail("LOOK-N-FIT", "Thank you for contacting us!", "admin@example.com", [from_email])
+                send_mail(subject, message,from_email,recipient_email, fail_silently=False)
+                send_mail("LOOK-N-FIT", "Thank you for contacting us!", settings.EMAIL_HOST_USER, [from_email], fail_silently=False)
             except:
                 return HttpResponse('Invalid header found.')
             return redirect('home')
